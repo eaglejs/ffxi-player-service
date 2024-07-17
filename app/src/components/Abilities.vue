befdba2ecad4e7f6c583483d9420708cd6e850be
Joshua S. Eagle <JoshuaSEagle@gmail.com>
2024-07-17 09:48:59 -0400

    Abilities: Created a component that will display ability timers.

--- app/src/components/Abilities.vue
+++ app/src/components/Abilities.vue
@@ -1,34 +1,24 @@
 <template>
-  <div class="container-fluid mt-1">
-    <div class="row">
-      <AbilityVue class="col-2" v-for="(ability, index) in filteredAbilities" :key="ability.ability" :ability="ability" />
+  <div class="row">
+    <div class="col-12 row" v-if="abilities.length">
+      <h2>Abilities</h2>
+      <section class="col-2" v-for="ability in abilities" :key="ability.ability">
+        <p class="m-0">{{ ability.ability }}</p>
+        <p>{{ ability.recast }}</p>
+      </section>
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
-import { computed, defineProps, ref } from 'vue'
-import AbilityVue from '@/components/AbilityVue.vue'
-import moment from 'moment'
 import type { Ability } from '@/types/Ability'
 
-const props = defineProps({
-  abilities: Array<Ability>
-})
-
-const allAbilities = ref(props?.abilities || [])
-
-// Computed property to filter abilities
-const filteredAbilities = computed(() => {
-  return allAbilities.value.filter(ability => {
-    // Calculate the difference between the current time and the recast time
-    const diffInSeconds = Math.abs(moment().diff(moment.unix(ability.recast), 'seconds'))
-    // Filter out abilities where the difference is less than or equal to 0
-    return diffInSeconds > 0
-  });
-})
-
+const props = defineProps<{
+  abilities: Ability[]
+}>()
 
 </script>
 
-<style scoped lang="scss"></style>
+<style scoped lang="scss">
+
+</style>
