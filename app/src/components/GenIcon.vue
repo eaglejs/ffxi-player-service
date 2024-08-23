d52bb74daffe677601556852852cf4169c6d48f3
Joshua S. Eagle <JoshuaSEagle@gmail.com>
2024-08-23 00:22:29 -0400

    Created a Generic Icon component.

---
+++ app/src/components/GenIcon.vue
@@ -0,0 +1,41 @@
+<template>
+  <svg 
+    xmlns="http://www.w3.org/2000/svg"
+    xmlns:xlink="http://www.w3.org/1999/xlink"
+    aria-hidden="true"
+    class="iconify iconify--mdi"
+    :width="iconSize"
+    :height="iconSize" 
+    fill="currentColor"
+    role="img"
+    preserveAspectRatio="xMidYMid meet"
+  >
+    <path :d="icon" />
+  </svg>
+</template>
+
+<script setup lang="ts">
+import { computed } from 'vue'
+
+const props = defineProps({
+  icon: String,
+  size: {
+    type: String,
+    required: true,
+    validator: (value: string) => ['sm', 'md', 'lg'].includes(value)
+  }
+})
+
+const iconSize = computed(() => {
+  switch (props.size) {
+    case 'sm':
+      return '16px';
+    case 'md':
+      return '20px';
+    case 'lg':
+      return '24px';
+    default:
+      return '20px';
+  }
+})
+</script>
