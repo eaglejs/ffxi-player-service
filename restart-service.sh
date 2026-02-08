06483d3b9a35392e5fd4b0aee44e30c9ee61c2c5
Joshua S. Eagle <JoshuaSEagle@gmail.com>
2026-02-08 14:50:05 -0500

    Reorganized File Structure.

--- restart-service.sh
+++ restart-service.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 
 launchctl unload -w ~/Library/LaunchAgents/com.eaglejs.ffxi.node.plist
+cp com.eaglejs.ffxi.node.plist ~/Library/LaunchAgents/
 launchctl load -w ~/Library/LaunchAgents/com.eaglejs.ffxi.node.plist
