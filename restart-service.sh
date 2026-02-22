520be9a44fc4e74a9cc9e27620b2d94007bb211d
eaglejs <joshuseagle@gmail.com>
2026-02-22 16:10:11 -0500

    created launchctl file

--- restart-service.sh
+++ restart-service.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
-launchctl unload -w ~/Library/LaunchAgents/com.eaglejs.ffxi.node.plist
-cp com.eaglejs.ffxi.node.plist ~/Library/LaunchAgents/
-launchctl load -w ~/Library/LaunchAgents/com.eaglejs.ffxi.node.plist
+#launchctl unload -w ~/Library/LaunchAgents/com.eaglejs.ffxi.node.plist
+#cp com.eaglejs.ffxi.node.plist ~/Library/LaunchAgents/
+#launchctl load -w ~/Library/LaunchAgents/com.eaglejs.ffxi.node.plist
+
+launchctl unload -w ~/Library/LaunchAgents/io.eaglejs.ffxi.service.plist
+cp io.eaglejs.ffxi.service.plist ~/Library/LaunchAgents/
+launchctl load -w ~/Library/LaunchAgents/io.eaglejs.ffxi.service.plist
