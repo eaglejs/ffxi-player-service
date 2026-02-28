5bddb87e63f6f6aff14f39625cbecc23f2dcce9f
eaglejs <joshuseagle@gmail.com>
2026-02-27 22:38:55 -0500

    Updated configs.

--- app/src/helpers/config.ts
+++ app/src/helpers/config.ts
@@ -1,17 +1,17 @@
 
-const host = window.location.hostname
-const port = 80
+const host = import.meta.env.VITE_SERVER_URL || window.location.hostname
+const port = import.meta.env.VITE_PORT || 80
 const protocol = window.location.protocol
 const wsProtocol = protocol === 'https:' ? 'wss:' : 'ws:'
 const apiPath = import.meta.env.MODE === 'staging' || import.meta.env.PROD || port === 80
   ? `/api`
-  : `:${port}`;
+  : `:${port}/api`;
 // const wsPath = import.meta.env.MODE === 'staging' || import.meta.env.PROD
 //   ? `/ws/players`
 //   : `:${port + 1}/ws/players`;
 const wsPath = `/ws/players`;
 const fullUrl = `${protocol}//${host}${apiPath}`
-const fullWsUrl = `${wsProtocol}//${host}${wsPath}`
+const fullWsUrl = `${wsProtocol}//${host}${port ? `:${port}` : ''}${wsPath}`
 
 const iconsPath = import.meta.env.MODE === 'staging' || import.meta.env.PROD
   ? `/assets/`
